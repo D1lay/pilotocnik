186b068f0f2a1377bd0a6828c7b9e4d4ab837328
D1lay <118356245+D1lay@users.noreply.github.com>
2023-03-01 02:05:06 +0300

    Add files via upload

---
+++ 2nd.js
@@ -0,0 +1,49 @@
+const creationDateObj = {
+  year: 2023,
+  month: 2,
+  day: 29,
+  hours: 0,
+  minutes: 3,
+  seconds: 0
+};
+
+const creationDate = new Date(
+  creationDateObj.year,
+  creationDateObj.month,
+  creationDateObj.day,
+  creationDateObj.hours,
+  creationDateObj.minutes,
+  creationDateObj.seconds
+);
+
+const timeLeft = document.querySelector('#timeLeft');
+
+setInterval(() => {
+
+  const now = new Date();
+  const diffInMS = now - creationDate;
+  const diffAsDate = new Date(diffInMS);
+
+  const [
+    years,
+    months,
+    days,
+    hours,
+    minutes,
+    seconds
+  ] = diffAsDate
+      .toISOString()
+      .split('T')
+      .map(timePart => 
+        timePart
+          .split(/-|:|\./)
+          .map(str => +str)
+      )
+      .flat();
+
+  daysLeft.textContent = `${days - 1} дней `;
+  HoursLeft.textContent = `${hours} час`;
+  MinsLeft.textContent = `${minutes} минут`;
+  SecLeft.textContent = `${seconds} секунд`;
+
+}, 1000);
